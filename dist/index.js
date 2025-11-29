a9e2ef8bc0acf79eac3c70c68b16cd2cb35329d2
GeorgeFerguson2910 <georgewferguson@btinternet.com>
2025-11-29 15:09:26 +0000

    stopped tracking dist forgot to add to .env at the start lol

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-import app from './app.js';
-var PORT = process.env.PORT || 4000;
-var server = app.listen(PORT, function () {
-  console.log("App is listening to port " + PORT);
-});
-export default server;
--- dist/models/index.js
+++
@@ -1,6 +0,0 @@
-import User from "./user.js";
-import Admin from "./admin.js";
-export default {
-  User: User,
-  Admin: Admin,
-};
